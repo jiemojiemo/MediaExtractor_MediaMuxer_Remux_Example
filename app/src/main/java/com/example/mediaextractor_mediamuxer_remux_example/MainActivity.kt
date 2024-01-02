c87b2d208291574fca3ba92d233ed70f613e82a9
jiemojiemo <460706836@qq.com>
2024-01-02 17:23:46 +0800

    can mixing audio and video

--- app/src/main/java/com/example/mediaextractor_mediamuxer_remux_example/MainActivity.kt
+++ app/src/main/java/com/example/mediaextractor_mediamuxer_remux_example/MainActivity.kt
@@ -12,11 +12,13 @@ import android.widget.Button
 import android.widget.TextView
 import java.io.File
 import java.nio.ByteBuffer
+import kotlin.math.max
+import kotlin.math.min
 
 class MainActivity : AppCompatActivity() {
     lateinit var textViewInput: TextView
     lateinit var textViewOutput: TextView
-    lateinit var remuxButton: Button
+    lateinit var mixingButton: Button
     lateinit var extractVideoButton: Button
     lateinit var extractAudioButton: Button
 
@@ -26,14 +28,14 @@ class MainActivity : AppCompatActivity() {
 
         textViewInput = findViewById(R.id.textViewInput)
         textViewOutput = findViewById(R.id.textViewOutput)
-        remuxButton = findViewById(R.id.remuxButton)
+        mixingButton = findViewById(R.id.mixingButton)
         extractVideoButton = findViewById(R.id.extractVideoButton)
         extractAudioButton = findViewById(R.id.extractAudioButton)
 
-        val remuxOutputFileName = "remux_output.mp4"
-        val remuxOutputFilePath = File(externalCacheDir, remuxOutputFileName).absolutePath
-        remuxButton.setOnClickListener {
-            remux(remuxOutputFilePath)
+        val mixingOutputFileName = "mixing_output.mp4"
+        val mixingOutputFilePath = File(externalCacheDir, mixingOutputFileName).absolutePath
+        mixingButton.setOnClickListener {
+            mixAudioAndVideo(mixingOutputFilePath)
         }
 
         val extractVideoOutputName = "extract_video_output.mp4"
@@ -48,53 +50,6 @@ class MainActivity : AppCompatActivity() {
             extractAudio(extractAudioOutputPath)
         }
 
-    }
-
-
-    private fun remux(outputFilePath: String) {
-        val mediaExtractor = MediaExtractor()
-        try {
-            resources.openRawResourceFd(R.raw.testfile).use { fd ->
-                mediaExtractor.setDataSource(fd)
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        textViewInput.text = buildFileInfo(mediaExtractor)
-
-        val mediaMuxer = MediaMuxer(outputFilePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
-        val trackCount = mediaExtractor.trackCount
-        var maxInputSize = 0
-        for (i in 0 until trackCount){
-            val trackFormat = mediaExtractor.getTrackFormat(i)
-            val maxInputSizeFromThisTrack = trackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
-            if (maxInputSizeFromThisTrack > maxInputSize) {
-                maxInputSize = maxInputSizeFromThisTrack
-            }
-
-            mediaExtractor.selectTrack(i)
-            mediaMuxer.addTrack(trackFormat)
-        }
-
-        val inputBuffer = ByteBuffer.allocate(maxInputSize)
-        val bufferInfo = BufferInfo()
-        mediaMuxer.start()
-        while(true)
-        {
-            val isInputBufferEnd = getInputBufferFromExtractor(mediaExtractor, inputBuffer, bufferInfo)
-            if (isInputBufferEnd) {
-                break
-            }
-            mediaMuxer.writeSampleData(mediaExtractor.sampleTrackIndex, inputBuffer, bufferInfo)
-            mediaExtractor.advance()
-        }
-
-        mediaMuxer.stop()
-        mediaMuxer.release()
-        mediaExtractor.release()
-
-        textViewOutput.text = buildFileInfo(outputFilePath)
     }
 
     private fun extractVideo(outputFilePath: String){
@@ -121,6 +76,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 mediaExtractor.selectTrack(i)
                 mediaMuxer.addTrack(trackFormat)
+                break
             }
         }
 
@@ -191,6 +147,70 @@ class MainActivity : AppCompatActivity() {
         textViewOutput.text = buildFileInfo(outputFilePath)
     }
 
+    private fun mixAudioAndVideo(outputFilePath: String){
+        val videoExtractor = MediaExtractor()
+        val audioExtractor = MediaExtractor()
+
+        try {
+            resources.openRawResourceFd(R.raw.testfile).use { fd ->
+                videoExtractor.setDataSource(fd)
+            }
+
+            resources.openRawResourceFd(R.raw.music).use { fd ->
+                audioExtractor.setDataSource(fd)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        textViewInput.text = buildFileInfo(videoExtractor)
+
+        val mediaMuxer = MediaMuxer(outputFilePath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4)
+
+        videoExtractor.selectTrack(0)
+        val videoTrackFormat = videoExtractor.getTrackFormat(0)
+        val muxerVideoTrackIndex = mediaMuxer.addTrack(videoTrackFormat)
+
+        // audio track at 1 in this file
+        audioExtractor.selectTrack(1)
+        val audioTrackFormat = audioExtractor.getTrackFormat(1)
+        val muxerAudioTrackIndex = mediaMuxer.addTrack(audioTrackFormat)
+
+        val videoTrackDuration = videoTrackFormat.getLong(MediaFormat.KEY_DURATION)
+        val videoMaxInputSize = videoTrackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
+        val audioTrackDuration = audioTrackFormat.getLong(MediaFormat.KEY_DURATION)
+        val audioMaxInputSize = audioTrackFormat.getInteger(MediaFormat.KEY_MAX_INPUT_SIZE)
+        val targetDuration = min(videoTrackDuration, audioTrackDuration)
+        val targetMaxInputSize = max(videoMaxInputSize, audioMaxInputSize)
+        val inputVideoBuffer = ByteBuffer.allocate(targetMaxInputSize)
+        val bufferInfo = BufferInfo()
+
+        mediaMuxer.start()
+        while(true){
+            val isVideoInputEnd = getInputBufferFromExtractor(videoExtractor, inputVideoBuffer, bufferInfo)
+            if(isVideoInputEnd || bufferInfo.presentationTimeUs >= targetDuration){
+                break
+            }
+            mediaMuxer.writeSampleData(muxerVideoTrackIndex, inputVideoBuffer, bufferInfo)
+            videoExtractor.advance()
+        }
+
+        while(true){
+            val isAudioInputEnd = getInputBufferFromExtractor(audioExtractor, inputVideoBuffer, bufferInfo)
+            if(isAudioInputEnd || bufferInfo.presentationTimeUs >= targetDuration){
+                break
+            }
+            mediaMuxer.writeSampleData(muxerAudioTrackIndex, inputVideoBuffer, bufferInfo)
+            audioExtractor.advance()
+        }
+
+        mediaMuxer.stop()
+        mediaMuxer.release()
+        videoExtractor.release()
+        audioExtractor.release()
+        textViewOutput.text = buildFileInfo(outputFilePath)
+    }
+
     private fun isVideoTrack(mediaFormat: MediaFormat): Boolean{
         val mime = mediaFormat.getString(MediaFormat.KEY_MIME)
         return mime?.startsWith("video/") ?: false
@@ -211,12 +231,8 @@ class MainActivity : AppCompatActivity() {
             return true
         }
 
-        val trackIndex = mediaExtractor.sampleTrackIndex
-        val pts = mediaExtractor.sampleTime
-        Log.e("MainActivity", "trackIndex: $trackIndex, pts: $pts")
-
         bufferInfo.size = sampleSize
-        bufferInfo.presentationTimeUs = pts
+        bufferInfo.presentationTimeUs = mediaExtractor.sampleTime
         bufferInfo.offset = 0
         bufferInfo.flags = mediaExtractor.sampleFlags
 
